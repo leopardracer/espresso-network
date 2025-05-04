84a0a91d86b69db9ade8919ec4beffabf54003a4
leopardracer <leopardracer@users.noreply.github.com>
2025-05-04 07:41:15 +0000

    deploy: 549953b457f86d40788203d3d56514f92b976226

--- trait.impl/hotshot_events_service/events_source/trait.EventsSource.js
+++ trait.impl/hotshot_events_service/events_source/trait.EventsSource.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["hotshot_events_service",[]],["sequencer",[["impl&lt;N: ConnectedNetwork&lt;PubKey&gt;, P: SequencerPersistence, V: Versions&gt; EventsSource&lt;SeqTypes&gt; for <a class=\"struct\" href=\"sequencer/api/struct.ApiState.html\" title=\"struct sequencer::api::ApiState\">ApiState</a>&lt;N, P, V&gt;"]]]]);
+    var implementors = Object.fromEntries([["hotshot_events_service",[]],["sequencer",[["impl&lt;N: <a class=\"trait\" href=\"hotshot_types/traits/network/trait.ConnectedNetwork.html\" title=\"trait hotshot_types::traits::network::ConnectedNetwork\">ConnectedNetwork</a>&lt;PubKey&gt;, P: SequencerPersistence, V: <a class=\"trait\" href=\"hotshot_types/traits/node_implementation/trait.Versions.html\" title=\"trait hotshot_types::traits::node_implementation::Versions\">Versions</a>&gt; EventsSource&lt;SeqTypes&gt; for <a class=\"struct\" href=\"sequencer/api/struct.ApiState.html\" title=\"struct sequencer::api::ApiState\">ApiState</a>&lt;N, P, V&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[29,271]}
+//{"start":57,"fragment_lengths":[29,587]}
--- trait.impl/request_response/data_source/trait.DataSource.js
+++ trait.impl/request_response/data_source/trait.DataSource.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["sequencer",[["impl&lt;I: NodeImplementation&lt;SeqTypes&gt;, V: Versions, N: ConnectedNetwork&lt;PubKey&gt;, P: SequencerPersistence&gt; DataSource&lt;<a class=\"enum\" href=\"sequencer/request_response/request/enum.Request.html\" title=\"enum sequencer::request_response::request::Request\">Request</a>&gt; for <a class=\"struct\" href=\"sequencer/request_response/data_source/struct.DataSource.html\" title=\"struct sequencer::request_response::data_source::DataSource\">DataSource</a>&lt;I, V, N, P&gt;"]]]]);
+    var implementors = Object.fromEntries([["sequencer",[["impl&lt;I: <a class=\"trait\" href=\"hotshot_types/traits/node_implementation/trait.NodeImplementation.html\" title=\"trait hotshot_types::traits::node_implementation::NodeImplementation\">NodeImplementation</a>&lt;SeqTypes&gt;, V: <a class=\"trait\" href=\"hotshot_types/traits/node_implementation/trait.Versions.html\" title=\"trait hotshot_types::traits::node_implementation::Versions\">Versions</a>, N: <a class=\"trait\" href=\"hotshot_types/traits/network/trait.ConnectedNetwork.html\" title=\"trait hotshot_types::traits::network::ConnectedNetwork\">ConnectedNetwork</a>&lt;PubKey&gt;, P: SequencerPersistence&gt; DataSource&lt;<a class=\"enum\" href=\"sequencer/request_response/request/enum.Request.html\" title=\"enum sequencer::request_response::request::Request\">Request</a>&gt; for <a class=\"struct\" href=\"sequencer/request_response/data_source/struct.DataSource.html\" title=\"struct sequencer::request_response::data_source::DataSource\">DataSource</a>&lt;I, V, N, P&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[511]}
+//{"start":57,"fragment_lengths":[1009]}
